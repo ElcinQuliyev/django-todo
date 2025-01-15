e4f080af3cd4ef8469f6544f3b5b67fe0d5245bc
Elcin Quliyev <elcinq743@gmail.com>
2025-01-15 22:04:55 +0400

    add: create register view [ 'GET', 'POST']

--- todo_list/accounts/views.py
+++ todo_list/accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
+from .forms import RegisterForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, logout
 # Create your views here.
+
+def register_view(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render(request, 'html/register.html', {'form':form})
+
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST) 
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'Singed up successfully.')
+            login(request, user)
+            return redirect('/home')
+        else:
+            return render(request, 'html/register.html', {'form': form})
+
+def sign_in_view(request):
+    ...
+
+
+def sign_out_view(request):
+    ...
+
+
+def sign_up_view(request):
+    ...
+
