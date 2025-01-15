a1da352b95369835e1382ace3c9cc35d17557b95
Elcin Quliyev <elcinq743@gmail.com>
2025-01-15 22:06:45 +0400

    add: create urls.py and path  ( register/ )

---
+++ todo_list/accounts/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import CustomUser
+from django.contrib.auth.forms import UserCreationForm
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=255)
+    avatar_img = forms.URLField()
+    password = forms.CharField(max_length=255, widget=forms.PasswordInput)
+    
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model=CustomUser
+        fields = [
+            'username', 
+            'avatar_img', 
+            'email', 
+            'password1', 
+            'password2'
+        ] 
+   
