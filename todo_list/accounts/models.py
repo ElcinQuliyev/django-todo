4bffbfa36e36ca736b060e7dee6630750d714120
Elcin Quliyev <elcinq743@gmail.com>
2025-01-15 12:58:30 +0400

    add: accounts  / models -> CustomUser

--- todo_list/accounts/models.py
+++ todo_list/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    avatar_img = models.URLField(max_length=255, blank=True, null=True)
+    level = models.PositiveSmallIntegerField(default=0)
+    xp = models.PositiveSmallIntegerField(default=0)
+    strength = models.PositiveSmallIntegerField(default=10)
+    intelligence = models.PositiveSmallIntegerField(default=10)
+    creativity = models.PositiveSmallIntegerField(default=10)
+    coins = models.PositiveSmallIntegerField(default=0)
+     
