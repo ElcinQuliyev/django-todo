2e3750cd52d5ed5a0541648e2480304f7dd5af1a
Elcin Quliyev <elcinq743@gmail.com>
2025-01-21 00:17:57 +0400

    add: @login_required in todo_list_view

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from todo.models import TaskQuestions
+from todo.forms import TaskForm
 
 
 def home_view(request):
     return render(request, 'html/home.html')
 
 
+@login_required
 def todo_list_view(request):
-    return render(request, 'html/todo_list.html')
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('todo_list')
+    else:
+        form = TaskForm()
+
+    tasks = TaskQuestions.objects.filter(user=request.user)
+    return render(request, 'html/todo_list.html', {'form': form, 'tasks': tasks})
