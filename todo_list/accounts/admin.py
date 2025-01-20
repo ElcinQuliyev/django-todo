095afabbb1ba7406849363c3abce32f534ad478a
Elcin Quliyev <elcinq743@gmail.com>
2025-01-21 00:15:16 +0400

    add: admin customize UI

--- todo_list/accounts/admin.py
+++ todo_list/accounts/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = [
+        'username', 
+        'email', 
+        'avatar_img', 
+        'level', 
+        'xp', 
+        'strength', 
+        'intelligence', 
+        'creativity', 
+        'coins', 
+        'is_active', 
+        'is_staff'
+    ]
+    list_filter = ['is_active', 'is_staff', 'level']
+    search_fields = ['username', 'email']
+    ordering = ['username']
+    
+
+admin.site.register(CustomUser, CustomUserAdmin)
